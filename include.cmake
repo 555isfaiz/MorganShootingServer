b94a8fcf3ec202a54ec6791867ffa0d93b8190e2
555isfaiz <1244274837@qq.com>
2022-04-06 20:56:19 +0200

    add linux jni include

--- include.cmake
+++ include.cmake
@@ -3,8 +3,12 @@
 # INCLUDE_DIRECTORIES("C:\\Program Files\\Java\\jdk-13.0.1\\include\\win32")
 
 # macOS:
-INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include")
-INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include/darwin")
+# INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include")
+# INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include/darwin")
+
+# linux:
+INCLUDE_DIRECTORIES("~/.sdkman/candidates/java/18-open/include")
+INCLUDE_DIRECTORIES("~/.sdkman/candidates/java/18-open/include/linux")
 
 INCLUDE_DIRECTORIES("./jni")
 INCLUDE_DIRECTORIES("./utils")
