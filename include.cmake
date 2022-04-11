968c0e06ddbd5fba5156d70a3f9fb98ad817b99b
555isfaiz <1244274837@qq.com>
2022-04-11 14:36:07 +0200

    reformat bullet3d dependency

--- include.cmake
+++ include.cmake
@@ -3,12 +3,12 @@
 # INCLUDE_DIRECTORIES("C:\\Program Files\\Java\\jdk-13.0.1\\include\\win32")
 
 # macOS:
-# INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include")
-# INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include/darwin")
+INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include")
+INCLUDE_DIRECTORIES("/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/include/darwin")
 
 # linux:
-INCLUDE_DIRECTORIES("~/.sdkman/candidates/java/18-open/include")
-INCLUDE_DIRECTORIES("~/.sdkman/candidates/java/18-open/include/linux")
+INCLUDE_DIRECTORIES("~/.sdkman/candidates/java/current/include")
+INCLUDE_DIRECTORIES("~/.sdkman/candidates/java/current/include/linux")
 
 INCLUDE_DIRECTORIES("./jni")
 INCLUDE_DIRECTORIES("./utils")
@@ -16,21 +16,9 @@ INCLUDE_DIRECTORIES("./game")
 INCLUDE_DIRECTORIES("./game/physics")
 INCLUDE_DIRECTORIES("./game/tasks")
 INCLUDE_DIRECTORIES("./msg")
-INCLUDE_DIRECTORIES("./libs/bullet3")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Collision")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Collision/BroadPhaseCollision")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Collision/NarrowPhaseCollision")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Common")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Dynamics")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Geometry")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3OpenCL")
-# INCLUDE_DIRECTORIES("./libs/bullet3/Bullet3Serialize")
-# INCLUDE_DIRECTORIES("./libs/bullet3/BulletCollision")
-# INCLUDE_DIRECTORIES("./libs/bullet3/BulletDynamics")
-# INCLUDE_DIRECTORIES("./libs/bullet3/BulletInverseDynamics")
-# INCLUDE_DIRECTORIES("./libs/bullet3/BulletSoftBody")
-# INCLUDE_DIRECTORIES("./libs/bullet3/clew")
-# INCLUDE_DIRECTORIES("./libs/bullet3/LinearMath")
 
+# add_subdirectory("./libs/bullet3")
+
+INCLUDE_DIRECTORIES("./libs/bullet3/src")
 INCLUDE_DIRECTORIES("./libs/json")
 
